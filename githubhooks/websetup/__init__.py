041d7c14e57b319eb8a6ee8dd4252c736d930e7c
Kent Bower <kb@retailarchitects.com>
2016-12-20 06:23:08 -0500

    Initial code as generated by `gearbox quickstart githubhooks` after `pip install tg.devtools`

---
+++ githubhooks/websetup/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""Setup the githubhooks application"""
+
+import logging
+
+from githubhooks.config.environment import load_environment
+
+__all__ = ['setup_app']
+
+log = logging.getLogger(__name__)
+
+from .schema import setup_schema
+from .bootstrap import bootstrap
+
+
+def setup_app(command, conf, vars):
+    """Place any commands to setup githubhooks here"""
+    load_environment(conf.global_conf, conf.local_conf)
+    setup_schema(command, conf, vars)
+    bootstrap(command, conf, vars)
